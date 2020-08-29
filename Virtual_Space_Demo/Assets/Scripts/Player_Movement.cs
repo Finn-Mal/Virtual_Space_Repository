43309815a3f8541dfc6c89d4d813e3b379ff1342
Finn-Mal <68063595+Finn-Mal@users.noreply.github.com>
2020-08-29 13:57:40 +1000

    Some development

---
+++ Virtual_Space_Demo/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Variables for Basic player movement
+        private float InputX;
+        private float InputY;
+        public float Movement_Speed;
+        public float c_Gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
